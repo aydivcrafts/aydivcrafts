82083e98658270e48e6efbb41b231a45644a18e8
gopalakrishnan-r <rgopalakrishnanmca@gmail.com>
2021-08-14 12:12:23 +0530

    found year added

--- assets/js/product.js
+++ assets/js/product.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
             product_container += '<div class="row g-0">'
             $.each(data, function(key, value) {
                 product_container += '<div class="col-lg-4 col-md-6 col-sm-12">'
-                product_container += '<a class="portfolio-box" href="'+value.img+'" title="Project Name">'
+                product_container += '<a class="portfolio-box" href="'+value.img+'" title='+ value.product_name + '>'
                 product_container += '<img class="img-fluid" src="'+value.img+'" alt="..." />'
                 product_container += '<div class="portfolio-box-caption">'
                 product_container += '<div class="project-category text-white-50">Category</div>'
