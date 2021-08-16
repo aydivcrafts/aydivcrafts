a991e53e4e9af25b68e108d23b9af3ce0e610416
gopalakrishnan-r <rgopalakrishnanmca@gmail.com>
2021-08-16 18:49:29 +0530

    product page modified

--- assets/js/product.js
+++ assets/js/product.js
@@ -5,20 +5,15 @@ $(document).ready(function() {
 
             var product_container = '';
 
-            product_container += '<div class="row g-0">'
+            product_container += '<div class="row">'
             $.each(data, function(key, value) {
                 product_container += '<div class="col-lg-4 col-md-6 col-sm-12">'
-                product_container += '<a class="portfolio-box" href="'+value.img+'" title='+ value.product_name + '>'
-                product_container += '<img class="img-fluid" src="'+value.img+'" alt="..." />'
-                product_container += '<div class="portfolio-box-caption">'
-                product_container += '<div class="project-category text-white-50">Category</div>'
-                product_container += '<div class="project-name"><a href="'+value.url+'">'+ value.product_name + '</a></div>'
+                product_container += '<div class="container-img">'
+                product_container += '<a href="'+value.url+'"><img class="img-fluid image" src="'+value.img+'" alt="..." /></a>'
+                product_container += '<div class="middle">'
+                product_container += '<div class="text font-weight-bolder">' + value.product_name + '</div>'
                 product_container += '</div>'
-                product_container += '</a>'
-                // product_container += '<div class="mt-2 shadow-sm p-3 bg-white mb-5 bg-body">'
-                // product_container += '<h4>' + value.id + ') ' +'<a href="'+value.url+'">'+ value.product_name + '</a></h4>'
-                // product_container += '<br/>'
-                // product_container += '</div>'
+                product_container += '</div>'
                 product_container += '</div>'
             });
             product_container += '</div>'
